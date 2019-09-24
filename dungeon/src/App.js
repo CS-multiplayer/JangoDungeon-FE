aaaae222d7cdec99dfa58fdf314c448c93274dca
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-09-24 18:00:02 -0500

    added routes and logic in app component

--- dungeon/src/App.js
+++ dungeon/src/App.js
@@ -1,16 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 import Game from './components/game/Home.js'
-import Login from './components/Login.js';
+import NavBar from './components/NavBar.js';
 
 function App() {
-  const [logedIn, setLogedIn] = useState(localStorage.getItem("user"))
+  const [logedIn, setLogedIn] = useState(!!localStorage.getItem("key"))
   const [backendUrl, setBackendUrl] = useState('https://lambda-mud-test.herokuapp.com')
 
+  useEffect(() => {
+    if (localStorage.getItem('key')) {
+      setLogedIn(true)
+    } else {
+      setLogedIn(false)
+    }
+  })
   return (
     <div className="App">
 
-      {logedIn ? null : <Login />}
+      <NavBar logedIn={logedIn} backendUrl={backendUrl} />
       <div className="container">
         <h1>welcome to the Jango Rango Dungeon!</h1>
         <Game backendUrl={backendUrl} />
