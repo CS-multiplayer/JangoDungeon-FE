2f3cb98b451babb566da923790422d994d3bbd5b
Jeff Lapp <lappjeff20@gmail.com>
2019-09-24 19:17:29 -0400

    Set up input buttons

--- dungeon/src/components/game/GameInputs.js
+++ dungeon/src/components/game/GameInputs.js
@@ -1,12 +1,24 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
+import { faArrowUp, faArrowDown, faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 function GameInputs() {
 
     return (
         <div className="inputs">
-          <i class="fas fa-arrow-up"></i>
-          Inputs
+          N
+          <div>
+            <FontAwesomeIcon icon={faArrowUp} size="lg" />
+          </div>
+          <div className="left_right">
+            W
+            <FontAwesomeIcon icon={faArrowLeft} size="lg" />
+            <FontAwesomeIcon icon={faArrowRight} size="lg" />
+            E
+          </div>
+          <div>
+            <FontAwesomeIcon icon={faArrowDown} size="lg" />
+          </div>
+          S
         </div>
     );
 }
