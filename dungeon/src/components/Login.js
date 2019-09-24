eeb2a8032c2535ce8e9a8ea5a0ccd385f33dfafd
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-09-24 18:00:44 -0500

    modified login to work properly, added classes

--- dungeon/src/components/Login.js
+++ dungeon/src/components/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
-function Login() {
+function Login(props) {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
     const [password1, setPassword1] = useState('')
@@ -26,50 +27,72 @@ function Login() {
     }
     function handleSubmit(e) {
         e.preventDefault()
+        let endpoint = ''
+        let creds = {}
+        if (isRegister) {
+            endpoint = `${props.backendUrl}/api/registration/`
+            creds = { "username": username, "password1": password, "password2": password1 }
+        } else {
+            endpoint = `${props.backendUrl}/api/login/`
+            creds = { "username": username, "password": password }
+        }
+        console.log(endpoint)
+        console.log(creds)
+        axios
+            .post(`${endpoint}`, creds, { 'Content-Type': 'application/json' })
+            .then(res => {
+                console.log(res.data)
+                localStorage.setItem('key', res.data.key)
+            })
 
     }
 
 
     return (
-        <section>
-            <div>
-                <button onClick={toggleRegister}>
-                    {isRegister ? 'Login' : 'SignUp'}
-                </button>
+        <React.Fragment>
+            <div className='login-back'>
+
             </div>
-            <h1>{isRegister ? 'Register' : 'Login'}</h1>
+            <section className="login-page">
+                <div>
+                    <button onClick={toggleRegister}>
+                        {isRegister ? 'Login' : 'SignUp'}
+                    </button>
+                </div>
+                <h1>{isRegister ? 'Register' : 'Login'}</h1>
 
-            <form className='login-inputs' onSubmit={handleSubmit}>
-                <div>
-                    <label>Username</label>
-                    <input
-                        value={username}
-                        onChange={handleChange}
-                        name='username'
-                    />
-                </div>
-                <div>
-                    <label>Password</label>
-                    <input
-                        value={password}
-                        onChange={handleChange}
-                        name='password'
-                    />
-                </div>
-                {isRegister &&
+                <form className='login-inputs' onSubmit={handleSubmit}>
                     <div>
-                        <label>veryify password</label>
+                        <label>Username</label>
                         <input
-                            value={password1}
+                            value={username}
                             onChange={handleChange}
-                            name='password1'
+                            name='username'
                         />
-                    </div>}
-                <button type='Submit'>Submit</button>
-            </form>
+                    </div>
+                    <div>
+                        <label>Password</label>
+                        <input
+                            value={password}
+                            onChange={handleChange}
+                            name='password'
+                        />
+                    </div>
+                    {isRegister &&
+                        <div>
+                            <label>veryify password</label>
+                            <input
+                                value={password1}
+                                onChange={handleChange}
+                                name='password1'
+                            />
+                        </div>}
+                    <button type='Submit'>Submit</button>
+                </form>
 
 
-        </section >
+            </section >
+        </React.Fragment>
     );
 }
 
