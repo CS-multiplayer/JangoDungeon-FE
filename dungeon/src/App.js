3157a431598fb667b92fee379462ff07697de2d5
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-09-24 12:14:07 -0500

    added components to app and created app scss

--- dungeon/src/App.js
+++ dungeon/src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.scss';
+import Game from './components/game/Home.js'
+import Login from './components/Login.js';
 
 function App() {
+  const [logedIn, setLogedIn] = useState(localStorage.getItem("user"))
+  const [backendUrl, setBackendUrl] = useState('https://lambda-mud-test.herokuapp.com')
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {logedIn ? null : <Login />}
+      <div className="container">
+        <h1>welcome to the Jango Rango Dungeon!</h1>
+        <Game backendUrl={backendUrl} />
+      </div>
+
+
     </div>
   );
 }
